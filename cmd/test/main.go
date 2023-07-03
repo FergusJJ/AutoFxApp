ccba851c27ce0953d33752f1d928fb8938139b83
Fergus Johnson <69755142+FergusJJ@users.noreply.github.com>
2023-07-03 01:15:11 +0100

    ui test changes

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -2,35 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"math/rand"
 	"pollo/internal/app/ui"
 	"time"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
-
-/*
-	type ApiMonitorMessage struct {
-		CopyPID     string  `json:"copyPID"`
-		SymbolID    int     `json:"symbolID"`
-		Price       float64 `json:"price"`
-		Volume      int     `json:"volume"`
-		Direction   string  `json:"direction"`
-		MessageType string  `json:"type"` //close or open
-	}
-*/
-
-var messages = []string{
-	"this is message 1",
-	"this is message 2",
-	"this is message 3",
-	"this is message 4",
-	"this is message 5",
-	"this is message 6",
-	"this is message 7",
-}
 
 type displayData struct {
 	DID         int
@@ -41,10 +18,20 @@ type displayData struct {
 func main() {
 	app := tview.NewApplication()
 
-	feed := ui.NewFeed(5)
+	pages := tview.NewPages()
 
-	flex := tview.NewFlex().SetDirection(tview.FlexRow)
-	flex.AddItem(feed.Box, 0, 1, false)
+	// First page content
+	firstPage := tview.NewFlex().SetDirection(tview.FlexRow)
+	firstPageText := tview.NewTextView().SetText("First Page")
+	firstPage.AddItem(firstPageText, 0, 1, true)
+
+	// Second page content
+	secondPage := tview.NewFlex().SetDirection(tview.FlexRow)
+	feed := ui.NewFeed(5)
+	secondPage.AddItem(feed.Box, 0, 1, true)
+
+	pages.AddPage("first", firstPage, true, true)
+	pages.AddPage("second", secondPage, true, false)
 
 	go func() {
 		// Simulating messages being added to the feed
@@ -61,27 +48,15 @@ func main() {
 		// Close the application when pressing "ESC"
 		if event.Key() == tcell.KeyEscape {
 			app.Stop()
+		} else if event.Rune() == '1' {
+			pages.SwitchToPage("first")
+		} else if event.Rune() == '2' {
+			pages.SwitchToPage("second")
 		}
 		return event
 	})
 
-	if err := app.SetRoot(flex, true).Run(); err != nil {
+	if err := app.SetRoot(pages, true).Run(); err != nil {
 		panic(err)
 	}
-}
-
-func feedData() []displayData {
-	m1 := rand.Intn(7)
-	m2 := rand.Intn(7)
-	d1 := displayData{
-		DID:         rand.Int(),
-		DataMessage: messages[m1],
-		Green:       (int(math.Round(rand.Float64()))) == 1,
-	}
-	d2 := displayData{
-		DID:         rand.Int(),
-		DataMessage: messages[m2],
-		Green:       (int(math.Round(rand.Float64()))) == 0,
-	}
-	return []displayData{d1, d2}
 }
--- internal/app/ui/ui.go
+++ internal/app/ui/ui.go
@@ -1,30 +1,21 @@
 package ui
 
-/*
-Vertical Layout:
-1/10 - Need a line saying how to quit the app / The user that is currently copying / maybe other info
-6/10 - Need main table area which will show positons.
-3/10 - A feed displaying what is happening under the hood
+import "github.com/rivo/tview"
 
-*/
+func InitUi() *tview.Application {
+	var app = tview.NewApplication()
+	pages := tview.NewPages()
+	pages.AddPage("Loading", newLoadingScreen(), false, true)
+	return app
+}
 
-// var uiApp = tview.NewApplication()
+func newLoadingScreen() *tview.Flex {
+	flexLayout := tview.NewFlex()
+	flexLayout.SetDirection(tview.FlexRow)
+	flexLayout.SetBorder(false)
+	return flexLayout
+}
 
-// func InitUi(name string) {
-// 	pages := tview.NewPages()
-// 	MainUILayout := tview.NewFlex().SetDirection(tview.FlexRow)
+func newMainScreen() {
 
-// 	infoText := getInfoView(name)
-// 	table := newTable()
-// 	// feed := newFeed()
-// 	// feed.pushToFeed()
-// 	MainUILayout.SetDirection(tview.FlexRow).
-// 		AddItem(infoText, 0, 1, false).
-// 		AddItem(table.TableView.SetBorder(true), 0, 13, false).
-// 		// AddItem(feed.FeedView.SetBorder(true), 0, 6, false)
-
-// 	// pages.AddPage("Main UI", MainUILayout, true, true)
-// 	if err := uiApp.SetRoot(pages, true).EnableMouse(true).Run(); err != nil {
-// 		panic(err)
-// 	}
-// }
+}
